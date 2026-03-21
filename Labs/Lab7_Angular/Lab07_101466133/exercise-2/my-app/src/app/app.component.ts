6ab5715c3552a9eb62ea2a403fa9bee1af77bdfa
OlegSanitskii <olegsanitskii@gmail.com>
2026-03-21 17:32:22 -0400

    Week-10 Lab 07 – Angular

---
+++ Labs/Lab7_Angular/Lab07_101466133/exercise-2/my-app/src/app/app.component.ts
@@ -0,0 +1,12 @@
+import { Component } from '@angular/core';
+import { RouterOutlet } from '@angular/router';
+
+@Component({
+  selector: 'app-root',
+  imports: [RouterOutlet],
+  templateUrl: './app.component.html',
+  styleUrl: './app.component.css'
+})
+export class AppComponent {
+  title = 'World! I am Oleg Sanitskii 101466133 George Brown College';
+}
